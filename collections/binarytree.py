bbae2655d7efbbaaeb5eec71ccedb68615c48bf9
kaeden <2b11.1935529.kaedentan@gmail.com>
2021-01-27 08:18:10 +0800

    non-expression classes

---
+++ collections/binarytree.py
@@ -0,0 +1,25 @@
+class BinaryTree:
+    def __init__(self,key, leftTree = None, rightTree = None):
+        self.key = key
+        self.leftTree = leftTree
+        self.rightTree = rightTree
+    def setKey(self, key):
+        self.key = key
+    def getKey(self):
+        return self.key
+    def getLeftTree(self):
+        return self.leftTree
+    def getRightTree(self):
+        return self.rightTree
+    def insertLeft(self, key):
+        if self.leftTree == None:
+            self.leftTree = BinaryTree(key)
+        else:
+            t =BinaryTree(key)
+            self.leftTree , t.leftTree = t, self.leftTree
+    def insertRight(self, key):
+        if self.rightTree == None:
+            self.rightTree = BinaryTree(key)
+        else:
+            t =BinaryTree(key)
+            self.rightTree , t.rightTree = t, self.rightTree
---
+++ collections/stack.py
@@ -0,0 +1,11 @@
+class Stack:
+    def __init__(self):
+        self.stacklist = []
+    def push(self, value):
+        self.stacklist.append(value)
+    def pop(self):
+        return self.stacklist.pop(-1)
+    def get(self):
+        return self.stacklist[-1]
+    def __str__(self):
+        print(str(self.stacklist))
