33a99fdcd87fe52b23b2992853be3a894a1af360
2b11.1902257yeosheenhern <2b11.1902257yeosheenhern@gmail.com>
2021-02-01 10:38:08 +0800

    changed collections name to avoid confusion

---
+++ collection/BinaryTree.py
@@ -0,0 +1,31 @@
+class BinaryTree:
+    def __init__(self,key, leftTree = None, rightTree = None):
+        self.key = key
+        self.leftTree = leftTree
+        self.rightTree = rightTree
+
+    def set_key(self, key):
+        self.key = key
+
+    def get_key(self):
+        return self.key
+
+    def get_left_tree(self):
+        return self.leftTree
+
+    def get_right_tree(self):
+        return self.rightTree
+
+    def insert_left(self, key):
+        if self.leftTree == None:
+            self.leftTree = BinaryTree(key)
+        else:
+            t =BinaryTree(key)
+            self.leftTree , t.leftTree = t, self.leftTree
+
+    def insert_right(self, key):
+        if self.rightTree == None:
+            self.rightTree = BinaryTree(key)
+        else:
+            t =BinaryTree(key)
+            self.rightTree , t.rightTree = t, self.rightTree
---
+++ collection/__init__.py
@@ -0,0 +1,2 @@
+from collection.Stack import Stack 
+from collection.BinaryTree import BinaryTree
