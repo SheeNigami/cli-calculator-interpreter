1f5dfe76bfb0067c5caf248ae4f683488cce40d1
Kaeden <kaeden.19@ichats.sp.edu.sg>
2021-02-13 18:45:03 +0800

    minor UI change

--- CLInterface.py
+++ CLInterface.py
@@ -10,7 +10,7 @@ class CLInterface:
         print("*" * 65)
         print("* ST107 DSAA: Expression Evaluator & Sorter                     *")
         print("*" + ("-" * 63) + "*")
-        print("*  - Done by: Yeo Sheen Hern (1902257) & Kaedan Tan (19*****)   *")
+        print("*  - Done by: Yeo Sheen Hern (1902257) & Kaedan Tan (1935529)   *")
         print("*  - Class DIT/FT/2B/11                                         *")
         print("*" * 65)
 
